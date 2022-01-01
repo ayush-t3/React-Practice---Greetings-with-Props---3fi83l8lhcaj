b2201b54aa06f5789fec262b7045515bc989d97d
Ayush Tiwari <59754907+ayush-t3@users.noreply.github.com>
2022-01-01 15:05:09 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,6 @@
 // write code for Welcome component here
 import React from 'react';
-funtion Welcome(props){
+function Welcome(props){
 return(
 <h1>Hey ! {props.name}</h1>
   <h2>Welcome to Newton School.</h2>
